0f243460d9bcde3b3b01f9678d564c79dbd16a59
Arman Vaziri <46859622+armanvaziri@users.noreply.github.com>
2023-11-09 16:38:58 -0500

    Updated combobet logic

--- combobet.ts
+++ combobet.ts
@@ -2,24 +2,31 @@ import axios from 'axios';
 import { getUserInputYN } from './utils/input';
 import { ethers } from 'ethers'
 import { config } from 'dotenv';
-import LP_ABI from './abis/lp-abi.json';
-import PROXY_ABI from './abis/proxy-abi.json';
-import USDT_ABI from './abis/usdt-abi.json';
+import LP_ABI from './abis/azuro-lp-abi.json';
+import USDT_PROXY_ABI from './abis/usdt-proxy-abi.json';
 config();
 
-const SRC_CHAIN_ID = 137; // Polygon
-const DST_CHAIN_ID = 137; // Polygon
-const USDT_TO_BET = process.env.USDT_TO_BET!;
-const USDT_ADDRESS = '0x7FFB3d637014488b63fb9858E279385685AFc1e2' // USDT Proxy contract on Polygon
-const LP_ADDRESS = '0xf5c1B93171D1D812d125233df15F36FEe8f4BA45' // Azuro LP Proxy contract on Polygon
+// FILL IN VALUES HERE
+const USDT_TO_BET = "0.1" // Amount of USDT to bet
+const AFFILIATE = "0x3121e8d2a4f0F220e8C7C7c6D9a7046527A54B19"; // Azuro Revenue Share Wallet
+// bet #1
+const CONDITION_ID1 = "100100000000000015814887710000000000000267121454"; // Azuro Game Market Variables from the subgraph
+const OUTCOME_ID1 = "7629"; // Azuro Game Market Variables from the subgraph
+const CURRENT_ODDS1 = "1.843697041733"; // Azuro Game Market Variables from the subgraph
+// bet #2
+const CONDITION_ID2 = "100100000000000015814887910000000000000267035449"; // Azuro Game Market Variables from the subgraph
+const OUTCOME_ID2 = "4583"; // Azuro Game Market Variables from the subgraph
+const CURRENT_ODDS2 = "1.794134472219"; // Azuro Game Market Variables from the subgraph
+
+const USDT_ADDRESS = '0xc2132D05D31c914a87C6611C10748AEb04B58e8F' // USDT Proxy contract on Polygon
+const LP_ADDRESS = '0x7043E4e1c4045424858ECBCED80989FeAfC11B36' // Azuro LP contract on Polygon
+// const CORE_ADDRESS = '0xA40F8D69D412b79b49EAbdD5cf1b5706395bfCf7' // Azuro PrematchCore contract on Polygon
 const EXPRESS_ADDRESS = '0x92a4e8Bc6B92a2e1ced411f41013B5FE6BE07613' // Azuro BetExpress contract on Polygon
-const PROXY_ADDRESS = '0x200BD65A3189930634af857C72281abE63C3da5e' // Azuro ProxyFront contract on Polygon
 const wallet = new ethers.Wallet(process.env.WALLET_PRIVATE_KEY!);
 
 // Contract Interfaces
 const lpInterface = new ethers.Interface(LP_ABI);
-const proxyInterface = new ethers.Interface(PROXY_ABI);
-const usdtInterface = new ethers.Interface(USDT_ABI);
+const usdtProxyInterface = new ethers.Interface(USDT_PROXY_ABI);
 
 const axiosClient = axios.create({
   baseURL: process.env.PEAZE_API_URL,
@@ -42,61 +49,48 @@ function calculateMinOdds(currentOdds: any) {
 
 async function comboBetEstimateTx() {
 
-  // Azuro Game Market Variables from the subgraph
-  const conditionId1: any = "100100000000000015811616850000000000000263423119";
-  const outcomeId1: any = "2361";
-  // const currentOdds1: any = "1.835601489481";
-  const conditionId2: any = "100100000000000015811616850000000000000263423119";
-  const outcomeId2: any = "2361";
-  // const currentOdds2: any = "1.835601489481";
   const selections: {}[] = [
-    [conditionId1, outcomeId1],
-    [conditionId2, outcomeId2]
+    [CONDITION_ID1, OUTCOME_ID1],
+    [CONDITION_ID2, OUTCOME_ID2]
   ];
   
   // Bet variables
   const betAmount = ethers.parseUnits(USDT_TO_BET, 6);
   const deadline = Math.floor(Date.now() / 1000) + 2000;
-  const affiliate = "0x3121e8d2a4f0F220e8C7C7c6D9a7046527A54B19"; // BookieBot Revenue Share Wallet
-//   const minOdds = calculateMinOdds(currentOdds);
+  const minOdds = calculateMinOdds(CURRENT_ODDS1);
 
-  const encodedBetData = ethers.AbiCoder.defaultAbiCoder().encode(
+  const betData = ethers.AbiCoder.defaultAbiCoder().encode(
     [ 'tuple(uint256, uint64)[]' ], 
     [ selections ]
   );
 
-  const encodedBet = proxyInterface.encodeFunctionData('bet', [
-    LP_ADDRESS,
-    [
-      {
-        core: EXPRESS_ADDRESS, 
-        amount: betAmount,
-        expiresAt: deadline,
-        extraData: {
-          affiliate: affiliate, 
-          minOdds: 0, 
-          data: encodedBetData
-        }
-      }
-    ]
-  ])
+  const encodedBet = lpInterface.encodeFunctionData('betFor', [
+    wallet.address, 
+    EXPRESS_ADDRESS, 
+    betAmount, 
+    deadline, 
+    {
+      affiliate: AFFILIATE, 
+      data: betData, 
+      minOdds: minOdds
+    }
+  ]);
 
   const betTx = {
-    to: PROXY_ADDRESS,
+    to: LP_ADDRESS,
     data: encodedBet
-  }
+  };
 
   const approvalTx = {
     to: USDT_ADDRESS,
-    data: usdtInterface.encodeFunctionData('approve', [
-      PROXY_ADDRESS,
+    data: usdtProxyInterface.encodeFunctionData('approve', [
+      LP_ADDRESS,
       betAmount
     ])
   };
 
   const { data } = await axiosClient.post('/single-chain/estimate', {
-    sourceChain: SRC_CHAIN_ID,
-    destinationChain: DST_CHAIN_ID,
+    sourceChain: 137,
     sourceToken: USDT_ADDRESS,
     userAddress: wallet.address,
     tokenAmount: betAmount.toString(),
@@ -108,8 +102,8 @@ async function comboBetEstimateTx() {
 }
 
 async function main() {
-    console.log('-'.repeat(60));
-    console.log(`Azuro Protocol tx on Polygon`);
+    console.log('\n' + '-'.repeat(60));
+    console.log(`Azuro Protocol combo-bet on Polygon`);
     console.log('-'.repeat(60) + '\n');
   
     console.log('Getting tx estimate...' + '\n');
